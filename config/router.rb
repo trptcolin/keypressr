cf4e641a28f829f57e10ddeefbc9c8dbaeb725e4
Colin Jones <trptcolin@gmail.com>
2009-01-17 17:40:32 -0500

    linking in game from homepage

--- config/router.rb
+++ config/router.rb
@@ -44,6 +44,7 @@ Merb::Router.prepare do
   # This is fine for most cases.  If you're heavily using resource-based
   # routes, you may want to comment/remove this line to prevent
   # clients from calling your create or destroy actions with a GET
+  match('/game').to(:controller => 'main', :action => 'game').name(:game)
   match('/:language').to(:controller => 'main', :action => 'index').name(:home)
   default_routes
   
