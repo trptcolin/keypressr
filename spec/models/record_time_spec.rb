70fad9637b1410176dcf93324404a6d660da65ec
Colin Jones <trptcolin@gmail.com>
2009-01-28 23:37:23 -0500

    more specificity in record time specs

--- spec/models/record_time_spec.rb
+++ spec/models/record_time_spec.rb
@@ -3,17 +3,18 @@ require File.join( File.dirname(__FILE__), '..', "spec_helper" )
 describe RecordTime do
 
   def new_record(attrs = {})
-    # TODO: populate default hash with valid data
-    RecordTime.new({}.merge(attrs))
+    RecordTime.new({:duration => 150, :user_id => 1, :text_id => 1}.merge(attrs))
   end
 
   it "should have a duration" do 
     record_time = new_record(:duration => nil)
     record_time.should_not be_valid
+    record_time.errors[:duration].should_not be_nil
   end
 
   it "should have a speed" do 
     record_time = new_record(:speed => nil)
     record_time.should_not be_valid
+    record_time.errors[:speed].should_not be_nil
   end
 end
